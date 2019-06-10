742eb042d7aa154bef2cdfdf598f9f2b93ba83d9
qiufeihong2018 <15058301288@163.com>
2019-06-10 23:14:17 +0800

    验证token

--- app.js
+++ app.js
@@ -10,8 +10,20 @@ var config = {
     }
 }
 var app = new Koa()
-app.use(function *(next) {
+app.use(function* (next) {
     console.log(this.query)
+    var token = config.wechat.token
+    var signature = this.query.signature
+    var nonce = this.query.nonce
+    var timestamp = this.query.timestamp
+    var echostr = this.query.echostr
+    var str = [token, timestamp, nonce].sort().join('')
+    var sha = sha1(str)
+    if (sha === signature) {
+        this.body = echostr + ''
+    } else {
+        this.body = 'wrong'
+    }
 })
 
 app.listen(1414)
