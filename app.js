6e2fc97932983f1070777771c7a0131cd99a58c1
qiufeihong2018 <15058301288@163.com>
2019-06-10 23:31:34 +0800

    test2验证token

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var config = {
     wechat: {
         appID: 'wx8933c10f9e79a2ac',
         appSecret: '018dfccdb771b4a0ffb1d349d4040379',
-        token: 'imoocisareallyamzingplacetolearn'
+        token: 'myfristpublicwechat'
     }
 }
 var app = new Koa()
