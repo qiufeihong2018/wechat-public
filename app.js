76ed83625d5797e8e44c07a4ab3e1f355f343d39
qiufeihong2018 <15058301288@163.com>
2019-06-10 23:21:15 +0800

    test验证token

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ var app = new Koa()
 app.use(function* (next) {
     console.log(this.query)
     var token = config.wechat.token
-    var signature = this.query.signature
-    var nonce = this.query.nonce
     var timestamp = this.query.timestamp
+    var nonce = this.query.nonce
+
+    var signature = this.query.signature
     var echostr = this.query.echostr
     var str = [token, timestamp, nonce].sort().join('')
     var sha = sha1(str)
+    console.log(sha)
     if (sha === signature) {
         this.body = echostr + ''
     } else {
