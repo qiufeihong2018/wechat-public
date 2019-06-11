f0d6317410e4898135bd8feb02e5bde797d71ce4
qiufeihong2018 <15058301288@163.com>
2019-06-11 22:03:40 +0800

    获取access_token1

--- middlewares/check.js
+++ middlewares/check.js
@@ -1,6 +1,51 @@
 'use strict'
 var sha1 = require('sha1')
+// 票据
+function Wechat(config) {
+    var that = this
+    this.appID = config.appID
+    this.appSecret = config.appSecret
+    this.getAccessToken = config.getAccessToken
+    this.saveAccessToken = config.saveAccessToken
 
+    this.getAccessToken()
+        .then(function (data) {
+            try {
+                data = JSON.parse(data)
+            } catch (e) {
+                return that.updateAccessToken(data)
+            }
+            if (that.isValidAccessToken(data)) {
+                Promise.resolve(data)
+            } else {
+                return that.updateAccessToken()
+            }
+        })
+        .then(function (data) {
+            that.access_token = data.access_token
+            that.expires_in = data.expires_in
+            that.saveAccessToken(data)
+        })
+
+}
+
+Wechat.prototype.isValidAccessToken = function (data) {
+    if (!data || !data.access_token || !data.expires_in) {
+        return false
+    }
+    var access_token = data.access_token
+    var expires_in = data.expires_in
+    var now = (new Date().getTime())
+
+    if (now < expires_in) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+// 校验
 module.exports = function (config) {
     return function* (next) {
         console.log(this.query)
