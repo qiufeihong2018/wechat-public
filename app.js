6be9b6f15835fc92429f1fc1819aa111e4eba101
qiufeihong2018 <15058301288@163.com>
2019-06-11 21:35:44 +0800

    分离和抽象

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var Koa = require('koa')
 var sha1 = require('sha1')
+var check=require('./middlewares/check')
 var config = {
     wechat: {
         appID: 'wx8933c10f9e79a2ac',
@@ -10,23 +11,7 @@ var config = {
     }
 }
 var app = new Koa()
-app.use(function* (next) {
-    console.log(this.query)
-    var token = config.wechat.token
-    var timestamp = this.query.timestamp
-    var nonce = this.query.nonce
-
-    var signature = this.query.signature
-    var echostr = this.query.echostr
-    var str = [token, timestamp, nonce].sort().join('')
-    var sha = sha1(str)
-    console.log(sha)
-    if (sha === signature) {
-        this.body = echostr + ''
-    } else {
-        this.body = 'wrong'
-    }
-})
+app.use(check(config.wechat))
 
 app.listen(1414)
 console.log('listening:1414')
