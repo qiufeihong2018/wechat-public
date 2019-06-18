443a657e7e9a05aa43ea831dc9b3f9a8733e0e3f
qiufeihong2018 <15058301288@163.com>
2019-06-18 22:17:23 +0800

    对比

--- middlewares/check.js
+++ middlewares/check.js
@@ -1,15 +1,52 @@
 'use strict'
 var sha1 = require('sha1')
-// 获取并验证可读流的原始主体
-var getRawBody = require('raw-body')
-var Wechat = require('./wechat')
+// 票据
+function Wechat(config) {
+    var that = this
+    this.appID = config.appID
+    this.appSecret = config.appSecret
+    this.getAccessToken = config.getAccessToken
+    this.saveAccessToken = config.saveAccessToken
 
+    this.getAccessToken()
+        .then(function (data) {
+            try {
+                data = JSON.parse(data)
+            } catch (e) {
+                return that.updateAccessToken(data)
+            }
+            if (that.isValidAccessToken(data)) {
+                Promise.resolve(data)
+            } else {
+                return that.updateAccessToken()
+            }
+        })
+        .then(function (data) {
+            that.access_token = data.access_token
+            that.expires_in = data.expires_in
+            that.saveAccessToken(data)
+        })
+
+}
+
+Wechat.prototype.isValidAccessToken = function (data) {
+    if (!data || !data.access_token || !data.expires_in) {
+        return false
+    }
+    var access_token = data.access_token
+    var expires_in = data.expires_in
+    var now = (new Date().getTime())
+
+    if (now < expires_in) {
+        return true
+    } else {
+        return false
+    }
+
+}
 
 // 校验
 module.exports = function (config) {
-
-    // var wechat = new Wechat(config)
-
     return function* (next) {
         console.log(this.query)
         let token = config.token
@@ -21,27 +58,10 @@ module.exports = function (config) {
         let str = [token, timestamp, nonce].sort().join('')
         let sha = sha1(str)
         console.log(sha)
-
-
-        if (this.method === 'GET') {
-            if (sha === signature) {
-                this.body = echostr + ''
-            } else {
-                this.body = 'check error'
-            }
-        } 
-        else if (this.method === 'POST') {
-            if (sha !== signature) {
-                this.body = 'check error'
-                return false
-            }
-            var data = yield getRawBody(this.req, {
-                length: this.length,
-                limit: '1mb',
-                encoding: this.charset
-            })
-            console.log('data', data.toString())
-            var content = yield util.
+        if (sha === signature) {
+            this.body = echostr + ''
+        } else {
+            this.body = 'check error'
         }
     }
 
