c7efd2a2390155d17de69c648ac985604b433f03
qiufeihong2018 <15058301288@163.com>
2019-06-23 12:33:55 +0800

    bug:handler is not defined

--- middlewares/check.js
+++ middlewares/check.js
@@ -3,7 +3,7 @@ var sha1 = require('sha1')
 var getRawBody = require('raw-body')
 var Wechat = require('./wechat')
 var util = require('./util')
-module.exports = function (config) {
+module.exports = function (config, handler) {
     var wechat = new Wechat(config)
 
     return function* (next) {
