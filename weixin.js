94a7fddebb625c77b8613b193ceae7408ba7b6c1
qiufeihong2018 <15058301288@163.com>
2019-06-23 13:09:22 +0800

    bug:  不出现自动回复订阅信息

--- weixin.js
+++ weixin.js
@@ -1,14 +1,16 @@
 'use strict'
 
+// 自动回复
 exports.reply = function* (next) {
     var message = this.weixin
+
     if (message.MsgType === 'event') {
         if (message.Event === 'subscribe') {
             if (message.EventKey) {
                 console.log('扫二维码进来：' + message.EventKey + ' ' + message.ticket)
             }
             console.log('订阅')
-            this.body = '哈哈，您订阅了飞鸿的公众号\r\n' + '消息ID:' + message.MsgId
+            this.body = '哈哈，您订阅了飞鸿的公众号'
         } else if (message.Event === 'unsubscribe') {
             console.log('无情取关')
             this.body = ''
