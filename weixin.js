48df4b82bf84529ea9b23a97f02912beb866a848
qiufeihong2018 <15058301288@163.com>
2019-06-25 22:24:46 +0800

    feat:上传视频

--- weixin.js
+++ weixin.js
@@ -31,52 +31,49 @@ exports.reply = function* (next) {
         var content = message.Content
         var reply = `您说的${message.Content}太复杂了`
         if (content === '1') {
-            reply = '一帆丰顺'
-        } else if (content === '2') {
-            reply = '好事成双'
-        } else if (content === '3') {
-            reply = '三星报喜'
-        } else if (content === '4') {
-            reply = '四季发财'
-        } else if (content === '5') {
-            reply = '五福临门'
-        } else if (content === '6') {
-            reply = '六六大顺'
-        } else if (content === '7') {
-            reply = '七星高照'
-        } else if (content === '8') {
-            reply = '八方来财'
-        } else if (content === '9') {
-            reply = '九九同心'
-        } else if (content === '10') {
-            reply = '十全十美'
-        } else if (content === '11') {
             reply = [{
                 title: '我的博客',
                 description: '我的心路历程',
                 picUrl: 'https://user-gold-cdn.xitu.io/2019/3/19/16995c12657064e9?imageView2/1/w/180/h/180/q/85/format/webp/interlace/1',
                 url: 'http://www.qiufeihong.top/'
             }]
-        } else if (content === '12') {
+        } else if (content === '2') {
             reply = [{
                 title: '我的github',
                 description: '欢迎来到飞鸿的github',
                 picUrl: 'https://avatars1.githubusercontent.com/u/36500514?s=460&v=4',
                 url: 'https://github.com/qiufeihong2018'
             }]
-        } else if (content === '13') {
+        } else if (content === '3') {
             reply = [{
                 title: '我的掘金',
                 description: '欢迎来到飞鸿的掘金',
                 picUrl: 'https://user-gold-cdn.xitu.io/2019/3/19/16995c12657064e9?imageView2/1/w/180/h/180/q/85/format/webp/interlace/1',
                 url: 'https://juejin.im/user/5bf4d63cf265da61561ee241/posts'
             }]
-        } else if (content === '14') {
+        } else if (content === '4') {
             var data = yield wechatApi.uploadMaterial('image', `${__dirname}/7.jpg`)
             reply = {
                 type: 'image',
                 mediaId: data.media_id
             }
+        } else if (content === '5') {
+            var data = yield wechatApi.uploadMaterial('video', `${__dirname}/8.mp4`)
+            reply = {
+                type: 'video',
+                title: '视频',
+                description: '例子',
+                mediaId: data.media_id
+            }
+        } else if (content === '6') {
+        } else if (content === '7') {
+        } else if (content === '8') {
+        } else if (content === '9') {
+        } else if (content === '10') {
+        } else if (content === '11') {
+        } else if (content === '12') {
+        } else if (content === '13') {
+        } else if (content === '14') {
         }
         this.body = reply
 
